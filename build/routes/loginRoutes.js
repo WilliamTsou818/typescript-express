ba2f683086a4931d4ca29c8581194368d048e027
WilliamTsou818 <cccc1589@gmail.com>
2021-08-21 19:17:17 +0800

    feat: add protected route

--- build/routes/loginRoutes.js
+++ build/routes/loginRoutes.js
@@ -2,6 +2,14 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.router = void 0;
 var express_1 = require("express");
+function requireAuth(req, res, next) {
+    if (req.session && req.session.loggedIn) {
+        next();
+        return;
+    }
+    res.status(403);
+    res.send('Not permitted');
+}
 var router = express_1.Router();
 exports.router = router;
 router.get('/login', function (req, res) {
@@ -20,4 +28,11 @@ router.get('/', function (req, res) {
         return res.send("\n      <div>\n        <div>You are logged in!</div>\n        <a href=\"/logout\">Logout</a>\n      </div>\n    ");
     }
     return res.send("\n    <div>\n      <div>You are not logged in!</div>\n      <a href=\"/login\">Login</a>\n    </div>\n  ");
+});
+router.get('/logout', function (req, res) {
+    req.session = undefined;
+    return res.redirect('/');
+});
+router.get('/protected', requireAuth, function (req, res) {
+    res.send('Welcome to protected route, logged in user!');
 });
