042490fcaf3b6f542197bedacf041d6bd18ad1cd
WilliamTsou818 <cccc1589@gmail.com>
2021-08-24 14:01:11 +0800

    feat: add middleware logger

--- build/controllers/decorators/controller.js
+++ build/controllers/decorators/controller.js
@@ -1,19 +1,24 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
+var __spreadArray = (this && this.__spreadArray) || function (to, from) {
+    for (var i = 0, il = from.length, j = to.length; i < il; i++, j++)
+        to[j] = from[i];
+    return to;
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.controller = exports.router = void 0;
+exports.controller = void 0;
 require("reflect-metadata");
-var express_1 = __importDefault(require("express"));
-exports.router = express_1.default.Router();
+var AppRouter_1 = require("../../AppRouter");
+var MetadataKeys_1 = require("./MetadataKeys");
 function controller(routePrefix) {
     return function (target) {
+        var router = AppRouter_1.AppRouter.getInstance();
         for (var key in target.prototype) {
             var routeHandler = target.prototype[key];
-            var path = Reflect.getMetadata('path', target.prototype, key);
+            var path = Reflect.getMetadata(MetadataKeys_1.MetadataKeys.path, target.prototype, key);
+            var method = Reflect.getMetadata(MetadataKeys_1.MetadataKeys.method, target.prototype, key);
+            var middlewares = Reflect.getMetadata(MetadataKeys_1.MetadataKeys.middleware, target.prototype, key) || [];
             if (path) {
-                exports.router.get("" + routePrefix + path, routeHandler);
+                router[method].apply(router, __spreadArray(__spreadArray(["" + routePrefix + path], middlewares), [routeHandler]));
             }
         }
     };
