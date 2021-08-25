233ed09f16eef912e8c5bb890f6df7534496892a
WilliamTsou818 <cccc1589@gmail.com>
2021-08-25 16:24:36 +0800

    fix: remove logger function

--- src/controllers/LoginController.ts
+++ src/controllers/LoginController.ts
@@ -1,15 +1,9 @@
 import { Request, Response, NextFunction } from 'express'
 import { get, controller, use, post, bodyValidator } from './decorators'
 
-function logger(req: Request, res: Response, next: NextFunction) {
-  console.log('Request was made')
-  next()
-}
-
 @controller('/auth')
 class LoginController {
   @get('/login')
-  @use(logger)
   getLogin(req: Request, res: Response): void {
     res.render('login')
   }
